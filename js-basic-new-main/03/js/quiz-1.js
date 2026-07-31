eecf5041f75006fe36352cfbe2036b39d53d5d35
Kyle <prophet@hanyang.ac.kr>
2026-07-31 10:11:51 +0900

    변수와 자료형 그리고 연산자 - 도전! 응용 문제

---
+++ js-basic-new-main/03/js/quiz-1.js
@@ -0,0 +1,4 @@
+"use strict";
+
+let content = prompt("내용 입력: ", "JavaScript");
+document.writeln(content);
---
+++ js-basic-new-main/03/js/quiz-2.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let width = prompt("사각형의 가로 값: ");
+let height = prompt("사각형의 세로 값: ");
+alert(`주어진 사각형의 너비: ${width * height}`);
