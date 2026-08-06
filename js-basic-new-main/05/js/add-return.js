3d658cddfdef6af7eaa260598d2802982a1ede98
Kyle <propheta@yonsei.ac.kr>
2026-08-06 12:49:40 +0900

    함수와 이벤트 - 여러 번 사용할 수 있는 함수 만들기

--- js-basic-new-main/05/js/add-return.js
+++ js-basic-new-main/05/js/add-return.js
@@ -1,9 +1,8 @@
-var num1 = parseInt(prompt("첫 번째 숫자 : "));
-var num2 = parseInt(prompt("두 번째 숫자 : "));
-var result = addNumber(num1, num2);
+const num1 = Number.parseInt(prompt("첫 번째 숫자 : "));
+const num2 = Number.parseInt(prompt("두 번째 숫자 : "));
+let result = addNumber(num1, num2);
 alert("두 수를 더한 값은 " + result + "입니다.");
 
-function addNumber(a, b) { 			
-    var sum = a + b;
-    return sum;
+function addNumber(a, b) {
+    return a + b;
 }
