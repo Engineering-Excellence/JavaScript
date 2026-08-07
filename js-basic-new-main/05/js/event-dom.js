85d7cf051529b2f3f001b3243fb716ec35a1a584
Kyle <prophet@hanyang.ac.kr>
2026-08-07 16:51:35 +0900

    함수와 이벤트 - 이벤트 다루기

---
+++ js-basic-new-main/05/js/event-dom.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const coverImage = document.querySelector("#cover");
+coverImage.onclick = () => alert('눌렀습니다');
+coverImage.onmouseover = () => coverImage.style.border = "5px black solid";
+coverImage.onmouseout = () => coverImage.style.border = "";
